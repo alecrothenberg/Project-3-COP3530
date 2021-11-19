2114db7ee1d937b68c77a00b554a1e08cec79b48
LucasP78 <73602451+LucasP78@users.noreply.github.com>
2021-11-18 21:30:44 -0500

    Add files via upload

---
+++ Graph.cpp
@@ -0,0 +1,8 @@
+#include "Graph.h"
+
+Graph::Graph(int numActors) {
+    adjMatrix = new int[numActors][numActors];
+    
+
+
+}
---
+++ Graph.h
@@ -0,0 +1,16 @@
+#include <unordered_map>
+
+class Graph {
+private:
+    std::unordered_map<std::string, int>;
+    int* adjMatrix;
+
+public:
+    Graph(int numActors);
+
+
+
+
+
+};
+
