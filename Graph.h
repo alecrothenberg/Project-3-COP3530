bdf5904b388d51f78ada4408ce6eead1a121946c
alecrothenberg <alec.rothenberg@ufl.edu>
2021-11-22 18:59:47 -0500

    changed array to a 2d vector and intialized all values to 0

--- Graph.h
+++ Graph.h
@@ -1,11 +1,13 @@
 #include <unordered_map>
 #include <string>
+#include <vector>
 
 class Graph {
 private:
     std::unordered_map<std::string, int> actorNames;
     
-    int** adjMatrix;
+    // use vectors to avoid the const declaration issue with arrays
+    std::vector<std::vector<int>> adjMatrix;//(rows, std::vector<int>(cols, 0));
 
 public:
     Graph(int numActors);
