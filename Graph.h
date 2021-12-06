ac8b1ae09c0d4002cdda9820100792192bc650c3
alecrothenberg <73545166+alecrothenberg@users.noreply.github.com>
2021-12-05 21:49:45 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -4,160 +4,159 @@
 #include <iostream>
 #include <curl/curl.h>
 
-//https://stackoverflow.com/questions/2329571/c-libcurl-get-output-into-a-string, by The Quantum Physicist
-size_t curlToString(void* data, size_t size, size_t nmemb, std::string* s) {
-
-    /*
-      Pretty sure this just prepares a C-style string and
-      adds data to it with append, nothing too crazy
-    */
-    size_t newSize = size * nmemb;
-
-    /*
-      Not entirely sure if the try/catch is necessary,
-      but it works so I won't change it for now
-     */
-    try {
-        s->append((char*)data, newSize);
-    }
-    catch (std::bad_alloc& excpt) {
-        return 0;
-    }
-
-    return newSize;
-}
-
-
-// prints all the names
-// use this to return a vector using getCast and adjust the url every time
-
-
 class Graph {
 private:
-    std::unordered_map<std::string, int> actorNames;
 
-    // use vectors to avoid the const declaration issue with arrays
-    std::vector<std::vector<int>> adjMatrix;//(rows, std::vector<int>(cols, 0));
+    struct Node {
+
+        std::string vertex;
+        int weight;
+
+        Node(std::string name, int val) {
+            vertex = name;
+            weight = val;
+        }
+
+        std::unordered_map<std::string, int> edges;
+
+    };
+
+    std::unordered_map<std::string, int> indexes;
+
+    std::unordered_map<int, std::unordered_map<std::string, int>> adjList; // adj list of all the actor names and then the unordered map at each actor name contains each actor they share an edge with and its weight
+    // weight is designated by how many movies theyve acted in togehter 
 
 
 
 public:
-    void parseAndFill(std::string jsString, int& index) {
-        std::size_t found = jsString.find("name");
-
-        if (found != std::string::npos) {
-
-            /*
-             * Find first instance of "name", erase the
-             * rest so I can find the right " char after
-             * and start at the beginning of the name
-            */
-            jsString.erase(0, found + 7);
-
-            //Find next " char that lies at the end of the name
-            found = jsString.find('\"');
-
-            actorNames[jsString.substr(0, found)] = index;
+    //Graph();
+    //void assignCastEdges(std::string csvLine, int& index);
+    //int find(std::string name, std::vector<Node>);
 
 
 
-            /*
-             * Get rid of content before name and up to
-             * "original_name" section so that find doesn't find it
-             */
 
-            jsString.erase(0, found + 15);
-            index++;
-            parseAndFill(jsString, index);
-        }
-    }
+    // write function that based on a csv file that has end lines at end of every cast
+    // assign each cast member an index in the map
+    // pass in the index as well
+    // push entire cast into a local vector and iterate through the cast vector
+    // for every cast member have a secodn vector to iterate through the cast again and place edges in the corresponding indexes
+    // write seperate function that iterates over the csv
+    // first funciton just takes in a string line
 
+    int find(std::string name, std::vector<Node> list) { //passed in the vect, see if already have edge
 
-    void apiCall(const char* apiAddress) {
-        //This would probably be in an API call function, but for now its just the main
-        CURL* curl;
-        CURLcode result;
-
-        curl_global_init(CURL_GLOBAL_DEFAULT);
-
-        curl = curl_easy_init();
-
-        if (curl) {
-            std::string dataString;
-            curl_easy_setopt(curl, CURLOPT_URL, apiAddress);
-
-            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L); //only for https
-            curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L); //only for https
-
-            //Prepping the curl data for the string and assigning output to dataString
-            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curlToString);
-            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &dataString);
-
-            result = curl_easy_perform(curl);
-
-            //Fuck with us and then we tweakin hoe (tweakin hoe)
-            int index = 0;
-            parseAndFill(dataString, index);
-
-            //Testing getCast
-            /*for (const auto& person : movieCast) {
-                std::cout << person << std::endl;
+        for (int i = 0; i < list.size(); ++i) {
+            if (list.at(i).vertex == name) {
+                return i;
             }
-            */
-            // Error check
-            if (result != CURLE_OK) {
-                std::cerr << "Error during curl request: "
-                          << curl_easy_strerror(result) << std::endl;
+        }
+
+        return -1;
+    }
+
+
+    void assignCastEdges(std::string csvLine, int& index) {
+
+        // get each cast members name
+        // parse csvLine and delete names and first comma
+
+        std::vector<std::string> castMembers;
+
+        while (csvLine.size() != 0) {
+            std::string castName;
+            int commaIndex = csvLine.find(',');
+            castName = csvLine.substr(0, commaIndex);
+            castMembers.push_back(castName);
+            //if (adjList.count(castName) == 0) {
+            //    adjList[castName]
+            //    ++index;
+            //}
+
+            csvLine.erase(0, commaIndex + 1);
+        }
+
+        // insert edges
+        // iterate through cast members and add them to the nodes list of the actor at i
+
+
+       /* for (int i = 0; i < castMembers.size(); ++i) {
+            int currentCast = indexes[castMembers.at(i)];
+            for (int j = 0; j < castMembers.size(); ++j) {
+                if (castMembers.at(i) == castMembers.at(j)) {
+                    continue;
+                }
+                else {
+
+                    if (find(castMembers.at(j), adjList[currentCast]) == -1) {
+                        adjList[currentCast].push_back(Node(castMembers.at(j), 1));
+                    }
+                    else {
+                        adjList[currentCast].at(find(castMembers.at(j), adjList[currentCast])).weight++;
+                    }
+
+                }
             }
+        }*/
 
-            curl_easy_cleanup(curl);
-        }
-        else {
-            std::cerr << "Error initializing curl." << std::endl;
+
+        // double for loop iterating through the cast vector
+
+        for (int i = 0; i < castMembers.size(); ++i) {
+            int currentCast = indexes[castMembers.at(i)];
+            for (int j = 0; j < castMembers.size(); ++j) {
+                if (castMembers.at(i) == castMembers.at(j)) {
+                    continue;
+                }
+                else
+                    adjList[currentCast][castMembers.at(j)]++;
+            }
         }
 
-        curl_global_cleanup();
+
+
     }
 
-    void actorMap() {
-
-        //insert a number starting at 62 right after movie/
-
-        std::cout << "Making API calls..." << std::endl;
-        for (int i = 62; i < 5000; ++i) {
 
 
-            // string find returns start of movie/
-            // add 5 to index of find to insert
-            std::string apiAddress = "https://api.themoviedb.org/3/movie//credits?api_key=c2cb38dac2b28f5c96780f1f9a396944";
-            int startofMovie = apiAddress.find("movie/");
-            const char* API;
-            std::string iString = std::to_string(i);
-            //apiAddress.erase(startofMovie + 6, iString.size());
-            apiAddress.insert(startofMovie + 6, iString);
-            API = apiAddress.c_str();
-
-            apiCall(API);
-        }
-    }
-
-    Graph(){
+    Graph() {
         // need to intialize the 2D array to contain all 0's to start
 
-        actorMap();
+       // actorNames = actorMap();
+        //this->adjMatrix = new int[102775][102];
 
-        std::cout << actorNames.size() << std::endl;
-        std::cout << "Resizing adjacency matrix..." << std::endl;
-        adjMatrix.resize(actorNames.size());
-        for (int i = 0; i < actorNames.size(); ++i) { // assigns every value to 0
-            adjMatrix.at(i).resize(actorNames.size(), 0);
+
+        std::ifstream casts("Moviecast_Data.csv");
+        std::string individualCast;
+        int index = 0;
+        while (getline(casts, individualCast)) {
+            assignCastEdges(individualCast, index);
         }
+        //assignCastEdges("joe,shmoe,bob,rob,", index);
+        //assignCastEdges("bob,robert,shrumpert,", index);
+        //assignCastEdges("bob,joe,shrumpert,", index);
+        std::cout << adjList.size() << std::endl;
+        std::cout << "Resizing adjacency matrix..." << std::endl;
+
+
+        //std::unordered_map<std::string, int> test = actorMap();
+
+       /* adjMatrix.resize(test.size());
+        for (int i = 0; i < test.size(); ++i) { // assigns every value to 0
+            adjMatrix.at(i).resize(test.size(), 0);
+        }
+        */
+
 
         //apiCall("https://api.themoviedb.org/3/movie/63/credits?api_key=c2cb38dac2b28f5c96780f1f9a396944");
-        // array is intialied
+        // array is intialied 
         // need to have a seperate function that places everything into the unordered map and assigns an index
         // probably need to change the constructor to no int taken in
+
+
     }
 
-};
 
+
+
+};
