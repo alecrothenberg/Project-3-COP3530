5535d3d6a60e3517a185325235c65be7d386f58d
alecrothenberg <73545166+alecrothenberg@users.noreply.github.com>
2021-12-05 22:19:11 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -25,7 +25,7 @@ private:
 
     std::unordered_map<std::string, int> indexes;
 
-    std::unordered_map<int, std::unordered_map<std::string, int>> adjList; // adj list of all the actor names and then the unordered map at each actor name contains each actor they share an edge with and its weight
+    std::unordered_map<int, std::unordered_map<int, int>> adjList; // adj list of all the actor names and then the unordered map at each actor name contains each actor they share an edge with and its weight
     // weight is designated by how many movies theyve acted in togehter 
 
 
@@ -70,10 +70,10 @@ public:
             int commaIndex = csvLine.find(',');
             castName = csvLine.substr(0, commaIndex);
             castMembers.push_back(castName);
-            //if (adjList.count(castName) == 0) {
-            //    adjList[castName]
-            //    ++index;
-            //}
+            if (indexes.count(castName) == 0) {
+                indexes[castName] = index;
+                ++index;
+            }
 
             csvLine.erase(0, commaIndex + 1);
         }
@@ -105,13 +105,14 @@ public:
         // double for loop iterating through the cast vector
 
         for (int i = 0; i < castMembers.size(); ++i) {
-            int currentCast = indexes[castMembers.at(i)];
+            int vertexFrom = indexes[castMembers.at(i)];
             for (int j = 0; j < castMembers.size(); ++j) {
-                if (castMembers.at(i) == castMembers.at(j)) {
+                int vertexTo = indexes[castMembers.at(j)];
+                if (vertexFrom == vertexTo) {
                     continue;
                 }
                 else
-                    adjList[currentCast][castMembers.at(j)]++;
+                    adjList[vertexFrom][vertexTo]++;
             }
         }
 
@@ -122,40 +123,21 @@ public:
 
 
     Graph() {
-        // need to intialize the 2D array to contain all 0's to start
-
-       // actorNames = actorMap();
-        //this->adjMatrix = new int[102775][102];
-
-
+        
         std::ifstream casts("Moviecast_Data.csv");
         std::string individualCast;
         int index = 0;
         while (getline(casts, individualCast)) {
             assignCastEdges(individualCast, index);
         }
-        //assignCastEdges("joe,shmoe,bob,rob,", index);
-        //assignCastEdges("bob,robert,shrumpert,", index);
-        //assignCastEdges("bob,joe,shrumpert,", index);
         std::cout << adjList.size() << std::endl;
         std::cout << "Resizing adjacency matrix..." << std::endl;
 
 
-        //std::unordered_map<std::string, int> test = actorMap();
-
-       /* adjMatrix.resize(test.size());
-        for (int i = 0; i < test.size(); ++i) { // assigns every value to 0
-            adjMatrix.at(i).resize(test.size(), 0);
-        }
-        */
+        
 
 
-        //apiCall("https://api.themoviedb.org/3/movie/63/credits?api_key=c2cb38dac2b28f5c96780f1f9a396944");
-        // array is intialied 
-        // need to have a seperate function that places everything into the unordered map and assigns an index
-        // probably need to change the constructor to no int taken in
-
-
+      
     }
 
 
