957257bf2065aaf41e01a7cd8633e02c3dce5df6
alecrothenberg <73545166+alecrothenberg@users.noreply.github.com>
2021-12-06 20:12:10 -0500

    Update Graph.h

--- Graph.h
+++ Graph.h
@@ -5,25 +5,16 @@
 #include <curl/curl.h>
 #include <sstream>
 #include <fstream>
+#include <iterator>
+#include <unordered_set>
 
 class Graph {
 private:
 
-    struct Node {
-
-        std::string vertex;
-        int weight;
-
-        Node(std::string name, int val) {
-            vertex = name;
-            weight = val;
-        }
-
-        std::unordered_map<std::string, int> edges;
-
-    };
+    
 
     std::unordered_map<std::string, int> indexes;
+    
 
     std::unordered_map<int, std::unordered_map<int, int>> adjList; // adj list of all the actor names and then the unordered map at each actor name contains each actor they share an edge with and its weight
     // weight is designated by how many movies theyve acted in togehter 
@@ -31,12 +22,6 @@ private:
 
 
 public:
-    //Graph();
-    //void assignCastEdges(std::string csvLine, int& index);
-    //int find(std::string name, std::vector<Node>);
-
-
-
 
     // write function that based on a csv file that has end lines at end of every cast
     // assign each cast member an index in the map
@@ -46,16 +31,7 @@ public:
     // write seperate function that iterates over the csv
     // first funciton just takes in a string line
 
-    int find(std::string name, std::vector<Node> list) { //passed in the vect, see if already have edge
-
-        for (int i = 0; i < list.size(); ++i) {
-            if (list.at(i).vertex == name) {
-                return i;
-            }
-        }
-
-        return -1;
-    }
+    
 
 
     void assignCastEdges(std::string csvLine, int& index) {
@@ -63,74 +39,75 @@ public:
         // get each cast members name
         // parse csvLine and delete names and first comma
 
-        std::vector<std::string> castMembers;
-
+        std::unordered_set<std::string> castMembers;
+        std::string castName;
+        int random = std::rand() % 102775;
         while (csvLine.size() != 0) {
-            std::string castName;
+            
             int commaIndex = csvLine.find(',');
             castName = csvLine.substr(0, commaIndex);
-            castMembers.push_back(castName);
-            if (indexes.count(castName) == 0) {
+            castMembers.insert(castName);
+            if (indexes.find(castName) == indexes.end()) {
                 indexes[castName] = index;
                 ++index;
+                
+                //auto itr = indexes.find(castName);
+                
             }
-
+            
+            //std::cout << castName << " " << index << std::endl;
             csvLine.erase(0, commaIndex + 1);
         }
+        //index += castMembers.size();
+        
+        //for (auto itr : castMembers) {
+        //    indexes[itr] = index;
+        //    ++index;
+        //}
 
         // insert edges
         // iterate through cast members and add them to the nodes list of the actor at i
 
 
-       /* for (int i = 0; i < castMembers.size(); ++i) {
-            int currentCast = indexes[castMembers.at(i)];
-            for (int j = 0; j < castMembers.size(); ++j) {
-                if (castMembers.at(i) == castMembers.at(j)) {
-                    continue;
-                }
-                else {
-
-                    if (find(castMembers.at(j), adjList[currentCast]) == -1) {
-                        adjList[currentCast].push_back(Node(castMembers.at(j), 1));
-                    }
-                    else {
-                        adjList[currentCast].at(find(castMembers.at(j), adjList[currentCast])).weight++;
-                    }
-
-                }
-            }
-        }*/
-
-
         // double for loop iterating through the cast vector
 
-        for (int i = 0; i < castMembers.size(); ++i) {
-            int vertexFrom = indexes[castMembers.at(i)];
-            for (int j = 0; j < castMembers.size(); ++j) {
-                int vertexTo = indexes[castMembers.at(j)];
-                if (vertexFrom == vertexTo) {
+        for (auto itr1 : castMembers) {
+            int vertexFrom = indexes[itr1];
+            for (auto itr2 : castMembers) {
+                int vertexTo = indexes[itr2];
+                if (castMembers.size() == 1) {
+                    adjList[vertexFrom][vertexTo]++;
+                }
+                else if (vertexFrom == vertexTo) {
                     continue;
                 }
                 else
                     adjList[vertexFrom][vertexTo]++;
             }
         }
+      
 
-
-
+       
+        //std::cout << index << std::endl;
     }
-
-
 
     Graph() {
         
         std::ifstream casts("Moviecast_Data.csv");
         std::string individualCast;
         int index = 0;
+        //getline(casts, individualCast);
         while (getline(casts, individualCast)) {
             assignCastEdges(individualCast, index);
         }
+        
         std::cout << adjList.size() << std::endl;
+        std::cout << indexes.size() << std::endl;
+
+        //std::cout << indexes["Bruce Willis"] << std::endl;
+        //std::cout << adjList.find(indexes["Bruce Willis"])->first << std::endl;
+        //std::cout << indexes["Susan Dyson"] << std::endl;
+        //std::cout << adjList.find(indexes["Susan Dyson"])->first << std::endl;
         std::cout << "Resizing adjacency matrix..." << std::endl;
 
 
