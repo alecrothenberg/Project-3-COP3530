71040b765377054b4ca16b7eece10404270cb5de
LucasP78 <73602451+LucasP78@users.noreply.github.com>
2021-12-09 01:28:34 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,9 +5,11 @@
 #include <wx/wx.h>
 #endif
 
+
 class App : public wxApp
 {
 public:
+    //Creates instance of a Frame for the app
     bool OnInit();
 };
 
@@ -16,128 +18,141 @@ class Frame : public wxFrame
 public:
     Frame(const wxString& title, const wxPoint& pos, const wxSize& size);
 
+    void boxInput(wxKeyEvent& event);
+
+    //Button functionality
+    void OnDijkstrasPush(wxCommandEvent& event);
+    void OnBFSPush(wxCommandEvent& event);
+    void OnDFSPush(wxCommandEvent& event);
+    
+    //Output aid
+    static std::string stringOutput(const std::pair<std::pair<long long, int>,
+            std::vector<std::string>>& data, const std::string& algName);
+
+    //Constructor organization
+    void makeBoxes();
+    void makeButtons();
+
 private:
 
     Graph g;
 
+    //ComboBox is a wxWidgets dropdown menu object
     wxComboBox* box1;
     wxComboBox* box2;
+
+    //Buttons
     wxButton* BFS;
     wxButton* Dijkstras;
     wxButton* DFS;
 
-    void OnExit(wxCommandEvent& event);
-    void OnAbout(wxCommandEvent& event);
-    void OnHello(wxCommandEvent& event);
-    void boxInput(wxKeyEvent& event);
 
-    void OnDijkstrasPush(wxCommandEvent& event);
-    void OnBFSPush(wxCommandEvent& event);
-    void OnDFSPush(wxCommandEvent& event);
-    static std::string stringOutput(const std::pair<std::pair<long long, int>,
-            std::vector<std::string>>& data, const std::string& algName);
-
-    void makeBoxes();
-    void makeButtons();
-
-
-
+//Declares a static event table for the Frame class
 wxDECLARE_EVENT_TABLE();
 };
 
+//Enumeration for the buttons, used in makeButtons()
 enum {
-    ID_Hello = 1,
-    ID_DIJKSTRAS = 2,
-    ID_BFS = 3,
-    ID_DFS = 4
+    ID_DIJKSTRAS = 1,
+    ID_BFS = 2,
+    ID_DFS = 3
 };
 
-
-// clang-format off
+//Table for event occurrences
 wxBEGIN_EVENT_TABLE(Frame, wxFrame)
-    EVT_MENU(ID_Hello, Frame::OnHello)
-    EVT_MENU(wxID_EXIT, Frame::OnExit)
-    EVT_MENU(wxID_ABOUT, Frame::OnAbout)
     EVT_BUTTON(ID_DIJKSTRAS, Frame::OnDijkstrasPush)
     EVT_BUTTON(ID_BFS, Frame::OnBFSPush)
     EVT_BUTTON(ID_DFS, Frame::OnDFSPush)
 wxEND_EVENT_TABLE()
 
+//Creates instance of the App class
 wxIMPLEMENT_APP(App);
-// clang-format on
 
+//                           APP FUNCTIONS
 bool App::OnInit()
 {
-    Frame* frame = new Frame("Hello World", wxPoint(50, 50), wxSize(450, 340));
+    Frame* frame = new Frame("Project 3", wxPoint(50, 50), wxSize(450, 250));
     frame->Show(true);
     return true;
 }
 
+
+//                          FRAME FUNCTIONS
+
 Frame::Frame(const wxString& title, const wxPoint& pos, const wxSize& size)
         : wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+
     auto menuFile = new wxMenu();
-    
-    //menuFile->Append(ID_Hello, "&Hello...\tCtrl-H",
-    //                 "Help string show in status bar");
-    //menuFile->AppendSeparator();
-    //menuFile->Append(ID_TEST, "&Test...\tCtrl-H", "Run Test");
-    //menuFile->AppendSeparator();
-    menuFile->Append(wxID_EXIT);
-
-    auto menuHelp = new wxMenu();
-    menuHelp->Append(wxID_ABOUT);
-
-    auto menuBar = new wxMenuBar();
-    menuBar->Append(menuFile, "&About");
-    menuBar->Append(menuHelp, "&Help");
-
 
     makeBoxes();
     makeButtons();
 
-    auto instructionText = new wxStaticText(this, wxID_ANY, "Please select two actors:", wxDefaultPosition,
+    //Creates static text variables
+    auto nameInstructions = new wxStaticText(this, wxID_ANY, "Please select cast or crew members:", wxPoint(110, 5),
                                             wxDefaultSize,wxALIGN_CENTER_HORIZONTAL, "iText");
+    auto buttonText = new wxStaticText(this, wxID_ANY, "Please select a traversal algorithm: ", wxPoint(113, 100),
+                                       wxDefaultSize,wxALIGN_CENTER_HORIZONTAL, "iText");
 
 
-    SetMenuBar(menuBar);
-    CreateStatusBar();
-    SetStatusText("Welcome to wxWidgets!");
+    //SetMenuBar(menuBar);
 }
 
-void Frame::OnExit(wxCommandEvent& event) { Close(true); }
+//Assigns dropdown box data
+void Frame::makeBoxes() {
+    box1 = new wxComboBox(this, wxID_ANY, _T("Select One"), wxPoint(70, 50), wxDefaultSize,
+                          0, NULL, wxTE_PROCESS_ENTER | wxTE_PROCESS_TAB);
+    box1->GetEventHandler()->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::boxInput));
+    box2 = new wxComboBox(this, wxID_ANY, _T("Select Another"), wxPoint(220, 50), wxDefaultSize,
+                          0, NULL, wxTE_PROCESS_ENTER | wxTE_PROCESS_TAB);
+    box2->GetEventHandler()->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::boxInput));
 
-void Frame::OnAbout(wxCommandEvent& event)
-{
-    wxMessageBox("This is a wxWidgets' Hello world example", "About Hello World",
-                 wxOK | wxICON_INFORMATION);
+    std::unordered_map<std::string, int> nameMap = g.getNames();
+    auto iter = nameMap.begin();
+
+    //Fills boxes with the names stored in the graph
+    for(; iter != nameMap.end(); ++iter){
+        box1->Append(iter->first);
+        box2->Append(iter->first);
+
+    }
 
 }
 
-void Frame::OnHello(wxCommandEvent& event)
-{
-    wxLogMessage("Hello World from wxWidgets!");
+//Assigns button positions/labels
+void Frame::makeButtons() {
+    Dijkstras = new wxButton(this, ID_DIJKSTRAS, _T("Dijkstra's Algorithm"),
+                             wxPoint(155, 130), wxDefaultSize, 0);
+    BFS = new wxButton(this, ID_BFS, _T("Breadth First Search"),
+                       wxPoint(152, 155), wxDefaultSize, 0);
+    DFS = new wxButton(this, ID_DFS, _T("Depth First Search"),
+                       wxPoint(157, 180), wxDefaultSize, 0);
 
 }
 
-
+//Traversal Button Code
 
 void Frame::OnDijkstrasPush(wxCommandEvent &event) {
+    //Takes current index of the name selected in the dropdown menu
     int fromTemp = box1->GetCurrentSelection();
     int toTemp = box2->GetCurrentSelection();
 
 
+    //Check if typed names are stored in the dropdown object
     if(fromTemp >= box1->GetCount() || toTemp >= box2->GetCount()){
-        wxMessageBox("Try checking spelling and capitalization.", "You have not entered a valid name!",
+        wxMessageBox("Try checking your spelling and capitalization.", "One or both of your names are invalid!",
                      wxOK | wxICON_NONE);
     }
     else{
+        //Get names that correspond w/ indices
         wxString fromWX = box1->GetString(fromTemp);
         wxString toWX = box2->GetString(toTemp);
 
+        //Convert wxStrings to cStrings
         std::string fromString = (const char*)fromWX.mb_str();
         std::string toString = (const char*)toWX.mb_str();
 
+        //Get traversal data
         std::pair<std::pair<long long, int>, std::vector<std::string>> dataPair = g.dijkstras(fromString, toString);
 
         wxMessageBox(stringOutput(dataPair, "Dijkstra's"),
@@ -146,6 +161,7 @@ void Frame::OnDijkstrasPush(wxCommandEvent &event) {
     }
 }
 
+//See OnDijkstrasPush
 void Frame::OnBFSPush(wxCommandEvent &event) {
     int fromTemp = box1->GetCurrentSelection();
     int toTemp = box2->GetCurrentSelection();
@@ -168,6 +184,7 @@ void Frame::OnBFSPush(wxCommandEvent &event) {
     }
 }
 
+//See OnDijkstrasPush
 void Frame::OnDFSPush(wxCommandEvent &event) {
     int fromTemp = box1->GetCurrentSelection();
     int toTemp = box2->GetCurrentSelection();
@@ -190,32 +207,23 @@ void Frame::OnDFSPush(wxCommandEvent &event) {
     }
 }
 
-void Frame::boxInput(wxKeyEvent &event) {
-
-    if(event.GetKeyCode() == WXK_TAB){
-        Navigate(wxNavigationKeyEvent::IsForward);
-    }
-    else{
-        event.Skip();
-    }
-
-}
-
+//Collects the traversal data and packages it with text
 std::string Frame::stringOutput(const std::pair<std::pair<long long, int>,
-        std::vector<std::string>>& data, const std::string& algName){
-
+    std::vector<std::string>>& data, const std::string& algName){
+    
     std::string message;
 
-    if(data.first.second == -1){
+    if(data.first.second == -1){//Disconnected flag
         message = "These people are not connected.";
     }
-    else if(data.first.second == 0){
+    else if(data.first.second == 0){//Same input flag
         message = "The people you have entered are the same person!";
     }
     else{
         message = "Nodes that separate the cast or crew members in this traversal: " + std::to_string(data.first.second)
                   + "\n The path found between cast and crew members with " + algName + ": \n";
 
+        //Adds names in path traversal to the string
         for(int i = data.second.size() - 1; i >= 0; i--){
             message += data.second[i];
             if(i > 0){
@@ -231,31 +239,14 @@ std::string Frame::stringOutput(const std::pair<std::pair<long long, int>,
     return message;
 }
 
-void Frame::makeBoxes() {
-    box1 = new wxComboBox(this, wxID_ANY, _T("Select One"), wxPoint(-1, 200), wxDefaultSize,
-                          0, NULL, wxTE_PROCESS_ENTER | wxTE_PROCESS_TAB);
-    box1->GetEventHandler()->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::boxInput));
-    box2 = new wxComboBox(this, wxID_ANY, _T("Select Another"), wxPoint(200, 200), wxDefaultSize,
-                          0, NULL, wxTE_PROCESS_ENTER | wxTE_PROCESS_TAB);
-    box2->GetEventHandler()->Connect(wxEVT_KEY_DOWN, wxKeyEventHandler(Frame::boxInput));
+//For scrolling in the dropdowns
+void Frame::boxInput(wxKeyEvent &event) {
 
-    std::unordered_map<std::string, int> nameMap = g.getNames();
-    auto iter = nameMap.begin();
-
-    for(; iter != nameMap.end(); ++iter){
-        box1->Append(iter->first);
-        box2->Append(iter->first);
-
+    if(event.GetKeyCode() == WXK_TAB){
+        Navigate(wxNavigationKeyEvent::IsForward);
     }
-
-}
-
-void Frame::makeButtons() {
-    Dijkstras = new wxButton(this, ID_DIJKSTRAS, _T("Dijkstra's Algorithm"),
-                             wxPoint(100, 70), wxDefaultSize, 0);
-    BFS = new wxButton(this, ID_BFS, _T("Breadth First Search"),
-                       wxPoint(100, 110), wxDefaultSize, 0);
-    DFS = new wxButton(this, ID_DFS, _T("Depth First Search"),
-                       wxPoint(100, 150), wxDefaultSize, 0);
+    else{
+        event.Skip();
+    }
 
 }
